d66894da34b3b8aa884c6d5418779968a06c9a1e
乌宏喆 <wuhongzhe@MacBook-Pro-2.local>
2026-08-02 13:44:32 +0800

    Add Google AdSense verification script

--- luckora-global/app/layout.tsx
+++ luckora-global/app/layout.tsx
@@ -30,6 +30,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          crossOrigin="anonymous"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1004666604396408"
+        />
+      </head>
       <body>
         <GoogleAnalytics />
         {children}
